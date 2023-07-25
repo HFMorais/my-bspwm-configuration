fbbfb8d37589ad20afc20e36c5dc28162dbb34a8
Henrique Morais <hfmorais@outlook.com>
2023-07-25 19:04:21 +0100

    Polybar bluetooth work
    Replaced bluetooth module with an working one.

---
+++ dotfiles/polybar/scripts/bluetooth.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
+then
+  
+  echo "%{F#4C566A}"
+else
+  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
+  then 
+    echo ""
+  fi
+  echo "%{F#EBCB8B}"
+fi
+
