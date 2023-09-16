3ca01d8d66dfdb832a404dd47ee43a35476f6dfc
Henrique Morais <hfmorais@outlook.com>
2023-09-16 15:35:29 +0100

    Even more private tweaks

--- dotfiles/my-tweaks/polybar/scripts/network.sh
+++ dotfiles/my-tweaks/polybar/scripts/network.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 output=""
 if ip link show | grep -q "enp6s0.*state UP"; then
-    output="%{F#3B4252} enp6s0"
+    output="%{T4}%{F#3B4252} %{T5}enp6s0"
 else
-    output="%{F#4C566A} Offline"
+    output="%{T4}%{F#4C566A} %{T5}Offline"
 fi
 
 if dpkg -l | grep -q "nordvpn"; then
